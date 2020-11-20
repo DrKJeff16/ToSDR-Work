6a4d31de16a51776d3f14d428991efd51d73a042
DrKJeff16 <g.maxc.fox@gmail.com>
2020-11-20 05:43:53 -0600

    Adding comments.

--- ToDo/Scripts/BASH/todo-to-done.sh
+++ ToDo/Scripts/BASH/todo-to-done.sh
@@ -2,4 +2,5 @@
 
 # This script is a prototype. DO NOT RUN UNLESS YOU ARE SURE ABOUT WHAT YOU'RE DOING!!!!!!!
 
-cp -r ToDo/
+rm -r Done/*                              # Removes all files inside the 'Done' Directory.
+cp -r ToDo/*                              # Copies all Files inside the 'ToDo' Folder to the 'Done' Folder.
