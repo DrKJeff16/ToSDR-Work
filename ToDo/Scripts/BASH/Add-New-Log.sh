93903c84963db6c029fabede911a4c200c622c12
DrKJeff16 <g.maxc.fox@gmail.com>
2020-11-20 08:20:15 -0600

    Usage Function Works!!!!

--- ToDo/Scripts/BASH/Add-New-Log.sh
+++ ToDo/Scripts/BASH/Add-New-Log.sh
@@ -3,25 +3,14 @@
             # DO NOT USE UNLESS IT'S MOVED TO THE ROOT FOLDER!
 # ----------------------------FUNCTIONS' SECTION--------------------------------
 usage(){                              # Prints quick usage instructions.
-  cat <<-EOF
-  ||==================================================================================================||
-  ||Usage:                                                                                            ||
-  ||      ./Add-New-Log.sh [-f | --file] <filename> [-d | --default] [-V | --version] [-h | --help]   ||
-  ||                                                                                                  ||
-  ||OPTIONS:==========================================================================================||
-  ||                                                                                                  ||
-  ||      -f  | --file        Prints to File.                                                         ||
-  ||      -d  | --default     Goes on Default Mode.                                                   ||
-  ||      -h  | --help        Prints Help.                                                            ||
-  ||==================================================================================================||
-  EOF
+  local txt=("||==================================================================================================||" "||Usage:                                                                                            ||" "||      ./Add-New-Log.sh [-f | --file] <filename> [-d | --default] [-V | --version] [-h | --help]   ||" "||                                                                                                  ||" "||OPTIONS:==========================================================================================||" "||                                                                                                  ||" "||      -f  | --file        Prints to File.                                                         ||" "||      -d  | --default     Goes on Default Mode.                                                   ||" "||      -h  | --help        Prints Help.                                                            ||" "||==================================================================================================||")
+  printf "%s\n" "${txt[@]}"
 }
 
-pr_help(){                            # Prints help.
-  cat <<-EOF
-    Add-New-Log.sh
-  EOF
+pr_hlp(){                            # Prints help.
+  echo "HELP ME"
 }
+
 # ------------------------------------------------------------------------------
 
 if [[ ${#} -eq 0 ]]; then             # Prints usage if no options/arguments are given.
@@ -31,7 +20,7 @@ fi
 while getopts :h arg
 do
   case $arg in
-    h) pr_help
+    h) pr_hlp
     ;;
     :) usage
     ;;
